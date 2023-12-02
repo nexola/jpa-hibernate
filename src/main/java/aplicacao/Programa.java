1d1c39c6872ac40bafba0c386496ee71bef2c647
Vitor Vianna <vitormatheusfv@gmail.com>
2023-12-02 13:33:39 -0300

    Persistindo dados com JPA e Hibernate

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -2,16 +2,38 @@ package aplicacao;
 
 import model.dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Programa {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1, "Marcos", "marcos@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Maria", "maria@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Amanda", "amanda@gmail.com");
+        Pessoa p1 = new Pessoa(null, "Marcos", "marcos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Maria", "maria@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Amanda", "amanda@gmail.com");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); // Nome criado no persistence.xml
+        EntityManager em = emf.createEntityManager();
+
+        // Persistindo objetos no banco de dados || CREATE
+        em.getTransaction().begin();
+//        em.persist(p1);
+//        em.persist(p2);
+//        em.persist(p3);
+
+        // Recuperando um objeto || READ
+        Pessoa p = em.find(Pessoa.class, 1);
+        // Removendo um objeto || DELETE
+//        em.remove(p);
+        System.out.println(p);
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+
+
 
     }
 }
--- src/main/java/model/dominio/Pessoa.java
+++ src/main/java/model/dominio/Pessoa.java
@@ -1,8 +1,16 @@
 package model.dominio;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
